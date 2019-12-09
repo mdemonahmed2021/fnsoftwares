e205e90526068b766760a7bf75a47fd7e8d69712
Emon <58401497+mdemonahmed2021@users.noreply.github.com>
2019-12-09 19:21:22 +0600

    Add files via upload

---
+++ design.js
@@ -0,0 +1,7 @@
+
+document.querySelector(".search-btn").addEventListener("click",function(){
+	document.querySelector(".search-opt-container").style.display = "block";
+});
+document.querySelector(".click-out").addEventListener("click",function(){
+	document.querySelector(".search-opt-container").style.display = "none";
+});
